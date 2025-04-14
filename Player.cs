37b244c9740e34fc1d6b212f7578087fe6c40667
Борислав Христов <hristovborislav45@gmail.com>
2025-04-14 18:30:41 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,5 +1,4 @@
-using UnityEngine;
-
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -13,23 +12,37 @@ public class PlayerController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public static float fireRateModifier = 1f;
+    private bool canMove = true;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        if (!canMove) return;
         if (Time.time >= nextFireTime)
         {
             Shoot();
             nextFireTime = Time.time + (fireRate * fireRateModifier);
+        };
+    }
+
+    public void StopMovement()
+    {
+        canMove = false;
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
         }
     }
 
     void FixedUpdate()
     {
+        if (!canMove) return;
         Vector3 forwardMovement = Vector3.right * forwardSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + forwardMovement);
 
@@ -38,11 +51,16 @@ public class PlayerController : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
             moveX = -((touch.position.x - Screen.width / 2f) / (Screen.width / 2f));
-
             moveX *= 0.7f;
         }
         Vector3 sideMovement = Vector3.forward * moveX * sideSpeed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + sideMovement);
+
+        Vector3 newPosition = rb.position + sideMovement;
+        float zMin = -5.5f;
+        float zMax = 5.5f;
+        newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+
+        rb.MovePosition(newPosition);
     }
 
     void Shoot()
