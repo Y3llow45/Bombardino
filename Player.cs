77bc519534034bfba75bb0119d39242b48fd7089
Борислав Христов <hristovborislav45@gmail.com>
2025-04-15 21:04:47 +0300

    Fix clones merging

--- Player.cs
+++ Player.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -14,11 +13,30 @@ public class PlayerController : MonoBehaviour
     public static float fireRateModifier = 1f;
     private bool canMove = true;
     private Animator animator;
+    private bool isMainPlayer = false;
+    private Vector3 initialOffset;
+    private Transform mainPlayerTransform;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        isMainPlayer = gameObject.name != "SoldierClone";
+
+        PlayerController[] players = FindObjectsOfType<PlayerController>();
+        foreach (var player in players)
+        {
+            if (player.isMainPlayer)
+            {
+                mainPlayerTransform = player.transform;
+                break;
+            }
+        }
+
+        if (!isMainPlayer)
+        {
+            initialOffset = transform.position - mainPlayerTransform.position;
+        }
     }
 
     void Update()
@@ -28,7 +46,7 @@ public class PlayerController : MonoBehaviour
         {
             Shoot();
             nextFireTime = Time.time + (fireRate * fireRateModifier);
-        };
+        }
     }
 
     public void StopMovement()
@@ -43,24 +61,46 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         if (!canMove) return;
+
         Vector3 forwardMovement = Vector3.right * forwardSpeed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + forwardMovement);
 
-        float moveX = 0f;
-        if (Input.touchCount > 0)
+        if (isMainPlayer)
         {
-            Touch touch = Input.GetTouch(0);
-            moveX = -((touch.position.x - Screen.width / 2f) / (Screen.width / 2f));
-            moveX *= 0.7f;
+            float moveX = 0f;
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                moveX = -((touch.position.x - Screen.width / 2f) / (Screen.width / 2f));
+                moveX *= 0.7f;
+            }
+            Vector3 sideMovement = Vector3.forward * moveX * sideSpeed * Time.fixedDeltaTime;
+
+            PlayerController[] allPlayers = FindObjectsOfType<PlayerController>();
+            float minOffsetZ = 0f;
+            float maxOffsetZ = 0f;
+            foreach (var player in allPlayers)
+            {
+                if (!player.isMainPlayer)
+                {
+                    float offsetZ = player.initialOffset.z;
+                    if (offsetZ < minOffsetZ) minOffsetZ = offsetZ;
+                    if (offsetZ > maxOffsetZ) maxOffsetZ = offsetZ;
+                }
+            }
+
+            float zMin = -8f - minOffsetZ;
+            float zMax = 8f - maxOffsetZ;
+            Vector3 newPosition = rb.position + forwardMovement + sideMovement;
+            newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
+            rb.MovePosition(newPosition);
         }
-        Vector3 sideMovement = Vector3.forward * moveX * sideSpeed * Time.fixedDeltaTime;
-
-        Vector3 newPosition = rb.position + sideMovement;
-        float zMin = -5.5f;
-        float zMax = 5.5f;
-        newPosition.z = Mathf.Clamp(newPosition.z, zMin, zMax);
-
-        rb.MovePosition(newPosition);
+        else
+        {
+            Vector3 targetPosition = mainPlayerTransform.position + initialOffset;
+            targetPosition.x = rb.position.x + forwardSpeed * Time.fixedDeltaTime;
+            targetPosition.z = Mathf.Clamp(targetPosition.z, -8f, 8f);
+            rb.MovePosition(targetPosition);
+        }
     }
 
     void Shoot()
