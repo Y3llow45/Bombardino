ef525a6c0272ebca7a456c9a2428857fdc9ca056
Борислав Христов <hristovborislav45@gmail.com>
2025-04-21 22:03:59 +0300

    Update WallController.cs

--- WallController.cs
+++ WallController.cs
@@ -3,37 +3,24 @@ using TMPro;
 
 public class WallController : MonoBehaviour
 {
-    public TextMeshPro numberText;
-    public TextMeshPro signText;
+    public TextMeshPro wallText;
     public Material wallGoodMaterial;
     public Material wallBadMaterial;
 
     private bool triggered = false;
-    private int cloneAmount;
-    private static int wallIndex = 0;
-
-    // Hardcoded number and sign for each wall
-    private static readonly (int number, int sign)[] wallValues = new (int, int)[]
-    {
-        (2, 1),  // Wall 0: +2
-        (1, -1), // Wall 1: -1
-        (3, 1),  // Wall 2: +3
-        (2, -1), // Wall 3: -2
-        (4, 1),  // Wall 4: +4
-        (3, -1)  // Wall 5: -3
-    };
 
     void Start()
     {
-        int currentIndex = wallIndex++;
-        // Get number and sign from array, default to last value if index exceeds array
-        (int number, int sign) = currentIndex < wallValues.Length ? wallValues[currentIndex] : wallValues[wallValues.Length - 1];
+        string text = wallText.text;
 
-        cloneAmount = sign * number;
-
-        signText.text = sign > 0 ? "+" : "-";
-        numberText.text = number.ToString();
-        GetComponent<Renderer>().material = sign > 0 ? wallGoodMaterial : wallBadMaterial;
+        if (text.StartsWith("+"))
+        {
+            GetComponent<Renderer>().material = wallGoodMaterial;
+        }
+        else if (text.StartsWith("-"))
+        {
+            GetComponent<Renderer>().material = wallBadMaterial;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -49,17 +36,14 @@ public class WallController : MonoBehaviour
         GameController gameController = FindObjectOfType<GameController>();
         if (gameController != null)
         {
-            int number = int.Parse(numberText.text);
-            if (signText.text == "+")
-                gameController.SpawnSoldiers(number);
-            else
-                gameController.RemoveSoldiers(number);
-        }
-    }
+            string text = wallText.text;
+            bool isPositive = text.StartsWith("+");
+            int num = int.Parse(text.Substring(1));
 
-    void OnDestroy()
-    {
-        if (wallIndex == FindObjectsOfType<WallController>().Length)
-            wallIndex = 0;
+            if (isPositive)
+                gameController.SpawnSoldiers(num);
+            else
+                gameController.RemoveSoldiers(num);
+        }
     }
 }
