0a9fcf2a4f0da645e016268c2fb5f27c3504b8a9
Борислав Христов <hristovborislav45@gmail.com>
2025-04-21 22:11:56 +0300

    Update CameraMove.cs

--- CameraMove.cs
+++ CameraMove.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset = new Vector3(0, 5, -10);
+    public Vector3 offset = new Vector3(-12, 5, 0);
 
     void Update()
     {
-        transform.position = player.position + offset;
+        transform.position = new Vector3(
+            player.position.x + offset.x,
+            player.position.y + offset.y,
+            player.position.z * 0.6f + offset.z
+        );
     }
 }
