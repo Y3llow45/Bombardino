1dee8600661d8e531e9665a0362063d20899384c
Борислав Христов <hristovborislav45@gmail.com>
2025-04-24 19:43:30 +0300

    Update BulletVisual.cs

--- BulletVisual.cs
+++ BulletVisual.cs
@@ -2,10 +2,50 @@ using UnityEngine;
 
 public class BulletVisual : MonoBehaviour
 {
-    public float lifetime = 1f;
+    public float lifetime = 0.5f;
+    private Vector3 lastPos;
 
     void Start()
     {
+        lastPos = transform.position;
         Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        Vector3 currentPos = transform.position;
+        Vector3 direction = currentPos - lastPos;
+        float distance = direction.magnitude;
+
+        if (distance > 0)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(lastPos, direction.normalized, out hit, distance))
+            {
+                if (hit.collider.CompareTag("Barrel"))
+                {
+                    BarrelController barrel = hit.collider.GetComponent<BarrelController>();
+                    if (barrel != null)
+                    {
+                        barrel.RegisterHit();
+                    }
+                }
+                if (hit.collider.CompareTag("Zombie"))
+                {
+                    Debug.Log("Hit HIM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    GameController gameController = FindObjectOfType<GameController>();
+                    if (gameController == null)
+                    {
+                        Debug.LogError("GameController not found in the scene!");
+                    }
+                    else
+                    {
+                        gameController.AddScore(10);
+                    }
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+        lastPos = currentPos;
     }
 }
